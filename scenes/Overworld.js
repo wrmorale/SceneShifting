7092a3f5d10349d67385c6d9f43a0b7f25d17f5a
Wrmorales <wrmorale@ucsc.edu>
2022-05-10 17:26:00 -0700

    Added what I could

--- scenes/Overworld.js
+++ scenes/Overworld.js
@@ -64,6 +64,18 @@ class Overworld extends Phaser.Scene {
                 this.physics.world.setBounds(this.ROOMWIDTH-this.player.displayWidth/2, 0, 
                     this.ROOMWIDTH+this.player.displayWidth, this.ROOMHEIGHT+this.player.displayHeight/2);
             }
+            if(blockedDown){
+                this.cameras.main.flash(250);
+                this.cameras.main.shake(250);
+                this.cameras.main.pan(
+                    this.ROOMWIDTH*1.5,
+                    this.ROOMHEIGHT*1.5,
+                    3000,
+                    'Cubic.easeOut'
+                );
+                this.physics.world.setBounds(this.ROOMWIDTH-this.player.displayWidth/2, 0, 
+                    this.ROOMWIDTH+this.player.displayWidth, this.ROOMHEIGHT+this.player.displayHeight/2);
+            }
         });
 
         // Use Phaser-provided cursor key creation function
